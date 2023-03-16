dd85d8561c255efa40231ed5374d9b8d48c65da0
AldanaAranda <aldanaaranda07@gmail.com>
2023-03-16 13:01:39 -0300

    ejercicio 3

---
+++ Guia3-Ejercicio3/src/guia3/ejercicio3/Guia3Ejercicio3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia3.ejercicio3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Guia3Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese una frase o una palabra");
+        String frase = leer.nextLine();
+        
+        if (frase.length() == 8) {
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
+    }
+    
+}
